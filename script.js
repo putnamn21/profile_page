34b3c153064ac891d9ec3073bc6d2b75bb1132d3
putnamn21 <nathanputnam2@gmail.com>
2016-03-23 09:53:02 -0600

    removed one parallax filter

--- script.js
+++ script.js
@@ -7,20 +7,14 @@ $(".projects-area").hide();
 var y;
 
 function scrollHandler (){
-    
-    //PARALLAX AREA
-    var parallaxSpeed = 5;
-    var parallaxPos = (y / parallaxSpeed);
-    var parallaxPos1 = (y / ( parallaxSpeed * 2 ));
   
-    var coordinates = '50% '+ parallaxPos + 'px';
+var parallaxPos = (y / 5);
+    //PARALLAX AREA
+  
+    var coordinates = '40% '+ parallaxPos + 'px';
     $(".home-parallax").css({backgroundPosition: coordinates});
   
-    var coordinates1 = '40% '+ parallaxPos1 + 'px';
-    $(".home-parallax-1").css({backgroundPosition: coordinates1});
-  
-
-    //social icons animation
+  //social icons animation
   
     if(y > 5){
       $(".navbar-scroll").css("animation-name", "iconscroll");
@@ -40,7 +34,8 @@ function scrollHandler (){
     if( y > 1300){
         $(".progress-bar").animate({ "width" : "show"});
       $("#progress-section").slideDown(800);
-    }  
+    }
+
 }
 
 $('#doneProject').click(function(){
